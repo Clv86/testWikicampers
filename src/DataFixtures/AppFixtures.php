ae7770f0c035c4b6eba8ddce946d43d4885fb9aa
Cl <charles-louis.velieu@tutanota.com>
2024-05-13 23:57:42 +0200

    Fixtures added for testing

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Vehicle;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+
+class AppFixtures extends Fixture
+{
+    private Generator $faker;
+
+    public function __construct()
+    {
+     $this->faker = Factory::create('fr_FR');
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+
+        for($i=0; $i<50; $i++){
+        $vehicle = new Vehicle();
+        $vehicle->setBrand($this->faker->word())
+            ->setModel($this->faker->word());
+
+        $manager->persist($vehicle);
+        }
+        $manager->flush();
+    }
+}
